d2f1774df596b04bf61980445728a954637f8de3
Miodrag Tokić <mtokic@gmail.com>
2012-11-24 15:57:34 +0100

    Added system info reader classes

---
+++ classes/sysinfo/core/system/reader/com.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Windows info reader
+ *
+ * @package    SysInfo
+ * @category   System
+ * @author     Miodrag Tokić <mtokic@gmail.com>
+ * @copyright  (c) 2012, Miodrag Tokić
+ * @license    MIT
+ */
+class SysInfo_Core_System_Reader_COM implements SysInfo_System_Reader {
+
+	// TODO
+}
